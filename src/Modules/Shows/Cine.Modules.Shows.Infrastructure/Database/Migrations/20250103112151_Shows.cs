483c7005704f2a773b52553d56687433d0dae261
Tomasz Kosowski <t.kosowski@hotmail.com>
2025-01-04 20:45:07 +0100

    Implement CreateShow

---
+++ src/Modules/Shows/Cine.Modules.Shows.Infrastructure/Database/Migrations/20250103112151_Shows.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cine.Modules.Shows.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Shows : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "Duration",
+                table: "Movies",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeOnly(0, 0, 0));
+
+            migrationBuilder.CreateTable(
+                name: "Shows",
+                columns: table => new
+                {
+                    ShowId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    HallId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MovieId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StartAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Duration = table.Column<TimeSpan>(type: "time", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shows", x => x.ShowId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Shows");
+
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Movies");
+        }
+    }
+}
